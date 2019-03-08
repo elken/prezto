35c6cabb23e538ce9c0ae3a60aa7b00fca7b4025
Ellis Kenyo <elken@mykolab.com>
2019-03-08 22:16:07 +0000

    thinkpad

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -50,6 +50,7 @@ alias _='sudo'
 alias b='${(z)BROWSER}'
 alias cp="${aliases[cp]:-cp} -i"
 alias e='${(z)EDITOR}'
+alias v='${(z)VISUAL}'
 alias ln="${aliases[ln]:-ln} -i"
 alias mkdir="${aliases[mkdir]:-mkdir} -p"
 alias mv="${aliases[mv]:-mv} -i"
